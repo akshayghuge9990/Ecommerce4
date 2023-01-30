1d5df54dab25d0be3f8278c60441a44c2397fec7
akshayghuge9990 <ghugeakshay123@gmail.com>
2023-01-30 17:56:07 +0530

    changes By Akshay Master

---
+++ src/main/java/com/akshay/EcommerceProject/EcommerceProject4Application.java
@@ -0,0 +1,13 @@
+package com.akshay.EcommerceProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceProject4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceProject4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshay/EcommerceProject/EcommerceProject4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.EcommerceProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProject4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
